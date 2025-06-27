ade1de2b325738bfc29e5c0df0cc86aee452c29d
May Cheong <maycheongs@gmail.com>
2025-06-27 15:11:00 -0500

    add projects with filler info

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import projects from '../../data/projects.json'
+import ProjectCard from '../ui/ProjectCard'
 export default function Projects () {
+    
     return (
-        <section></section>       
+        <section id="projects" className="projects">
+            <h2 className="section-title">Projects</h2>
+            <div className="projects-container">
+                {projects.map((project, index) => (
+                    <ProjectCard key={index} project={project} />
+                ))}
+            </div>
+        </section>       
     )
 }
--- src/components/ui/ProjectCard.jsx
+++ src/components/ui/ProjectCard.jsx
@@ -1,2 +1,26 @@
-import React from 'react';
-import '../styles/global.css';
+export default function ProjectCard({ project }) {
+
+    const { title, description, image, github, demo, stack } = project;
+    return (
+        <div className="project-card">
+            <img src={image} alt={title} className="project-image" />
+
+            <div className="project-details">
+                <div>
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                </div>
+
+                <div className="tech-stack">
+                    <span>Tech Stack:</span>
+                    <span>{stack.map((s,i)=> i == stack.length - 1  ? s : `${s},`)}</span>
+                </div>
+
+                <div className="project-links">
+                    {github && <a href={github} target="_blank">GitHub</a>}
+                    {demo && <a href={demo} target="_blank">Demo</a>}
+                </div>
+            </div>
+        </div>
+    );
+}
