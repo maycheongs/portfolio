24dfe42e7d5f484da1f94e54e1e3c2c325d0a1c1
May Cheong <maycheongs@gmail.com>
2025-06-27 10:06:18 -0500

    add button component

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,2 +1,13 @@
 import React from 'react';
 import '../styles/global.css';
+
+export default function Button({ text, onClick, className }) {
+    return (
+        <button
+            className={`btn ${className}`}
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    );
+}
