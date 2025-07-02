0349e72279926af7d6c4181960422764aaf305bb
May Cheong <maycheongs@gmail.com>
2025-07-02 14:58:20 -0500

    add edited contact me form to index

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,1 +1,12 @@
-import React from 'react';
+export default function Footer() {
+    return (
+
+        <footer>
+            <div class="footer">
+                <p>&copy; 2025 May C. Built with lots of coffee ☕</p>
+            </div>
+        </footer>
+
+
+    )
+}
