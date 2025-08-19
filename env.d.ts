5d84cf3bc68098e555df4e82028913c33408b326
May Cheong <maycheongs@gmail.com>
2025-08-19 14:13:47 -0500

    add env types for importMetaEnv for social links

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,9 @@
 interface ImportMetaEnv {
     readonly CONTENTFUL_SPACE_ID: string;
     readonly CONTENTFUL_ACCESS_TOKEN: string;
+    readonly PUBLIC_GITHUB_URL: string;
+    readonly PUBLIC_LINKEDIN_URL: string;
+    readonly PUBLIC_EMAIL: string;
 }
 
 interface ImportMeta {
